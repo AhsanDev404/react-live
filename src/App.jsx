afbdc142b910c1dd5cf7a08bb8fe9e7580d2d5ed
AhsanDev404 <ahsan.dev.404@gmail.com>
2023-07-04 19:43:20 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-
-import './App.css'
-
+import "./App.css";
 
 function App() {
- 
-
-
-
   return (
     <>
-    
-   <h1>Testing to deploy</h1>
+      <h1>Testing to deploy</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
